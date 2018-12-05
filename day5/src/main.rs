5d462d4d95e7955f9a606d4010c754ced03f3b7c
Suraj Malhotra <surajmalhotra@google.com>
2018-12-05 00:36:49 -0800

    day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 fn part1(input: &str) -> usize {
     let mut chemicals = String::from(input);
     eprintln!("chemicals_len: {}", chemicals.len());
+=======
+extern crate rayon;
+
+use rayon::prelude::*;
+
+fn part1(input: &str) -> usize {
+    let mut chemicals = String::from(input);
+>>>>>>> day 5
     loop {
 	let mut iter = chemicals.chars().peekable();
 	let mut accum = String::new();
@@ -25,7 +34,12 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
+<<<<<<< HEAD
     (b'a'..b'z').map(|c| c as char).map(|lowercase| {
+=======
+    let letters: Vec<_> = (b'a'..b'z').collect();
+    letters.par_iter().map(|c| *c as char).map(|lowercase| {
+>>>>>>> day 5
 	let uppercase = lowercase.to_ascii_uppercase();
 
 	let input = input.chars()
